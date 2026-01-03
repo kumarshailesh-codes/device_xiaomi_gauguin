dd32f7ad6e5d61709e9822f3f7f413f748d78877
kumaarshailesh-code <kumarshailesh12qw@gmail.com>
2026-01-03 11:56:10 +0530

    device: gauguin: add MistOS OTA updater overlay

--- device.mk
+++ device.mk
@@ -436,3 +436,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
     firmware_WCNSS_qcom_cfg.ini_symlink
+    
+# MistOS OTA overlay
+PRODUCT_PACKAGES += \
+    UpdaterResGauguinMistOS
+    
