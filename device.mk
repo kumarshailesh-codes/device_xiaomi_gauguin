a224f8c341d29dd58e7d28d02afdf25eb24b434e
kumarshailesh-codes <kumarshailesh12qw4@gmail.com>
2026-02-19 11:59:42 +0530

    gauguin: Rebrand device tree to Infinity OS
    * Update product makefiles and device overlays
    * Set infinity-specific build properties and maintainer info

--- device.mk
+++ device.mk
@@ -22,7 +22,7 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-infinity
 
 PRODUCT_PACKAGES += \
     GauguinApertureOverlay \
@@ -272,11 +272,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
+$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,true)
 
 # Media
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
+$(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
 
 # Native Public Libraries
 PRODUCT_COPY_FILES += \
--- infinity_gauguin.mk
+++ infinity_gauguin.mk
@@ -17,14 +17,23 @@ $(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := infinity_gauguin
 PRODUCT_DEVICE := gauguin
+PRODUCT_MODEL := Gauguin
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="gauguin-user 12 RKQ1.200826.002 V14.0.2.0.SJSMIXM release-keys" \
     BuildFingerprint=Xiaomi/gauguin_global/gauguin:12/RKQ1.200826.002/V14.0.2.0.SJSMIXM:user/release-keys
+    
+    DeviceProduct=gauguin \
+    SystemName=gauguin \
+    BuildFlavor="gauguin-user" \
+    BuildHostname="shailesh-build" \
+    BuildUsername="Shailesh"
 
 # Infinity X Maintainer
 INFINITY_MAINTAINER := "Shailesh"
@@ -33,5 +42,13 @@ INFINITY_MAINTAINER := "Shailesh"
 WITH_GAPPS := true
 
 # Device features
-TARGET_SUPPORTS_QUICK_TAP := true
+TORCH_STR_SUPPORTED := true
 TARGET_ENABLE_BLUR := true
+
+RELAX_USES_LIBRARY_CHECK := true
+MALLOC_SVELTE_FOR_LIBC32 := true
+BYPASS_CHARGE_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_INCLUDE_GOOGLE_DIALER := false
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+MAX_EGL_CACHE_SIZE := 2048*1024
